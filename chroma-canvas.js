42d45588fa214f64c083d4e0b5178e566b94dbfe
guysela567 <guysela567@gmail.com>
2022-12-28 01:50:10 +0200

    Improved visual interface

--- chroma-canvas.js
+++ chroma-canvas.js
@@ -1,14 +1,25 @@
-const rows = 6, height = 6;
-const cols = 22, width = 22;
-let canvas;
+const rows = 6, cols = 22;
 
+let canvas;
 let chromaSDK;
 let previewGrid = createPreviewGrid();
+let previewCanvas;
 
-function chromaInit(fps) {
+const prevC = (p) => {
+  p.setup = () => {
+    p.createCanvas(22 * scl, 6 * scl).parent('#main');
+  };
+
+  p.draw = () => {
+    p.image(canvas, 0, 0, p.width, p.height);
+  };
+};
+
+function chromaInit(fps, scl) {
   chromaSDK = new ChromaSDK();
   chromaSDK.init();
   canvas = createCanvas(cols, rows).hide();
+  previewCanvas = new p5(prevC);
   noStroke();
   frameRate(fps);
 }
@@ -24,10 +35,10 @@ function createPreviewGrid() {
     grid[i] = [];
     for (let j = 0; j < cols; j++) {
       const cell = document.createElement('div');
+      cell.innerHTML = KEYCAP_TEXT[i][j];
       wrapper.appendChild(cell);
       grid[i][j] = cell;
     }
-    wrapper.appendChild(document.createElement('br'));
   }
 
   return grid;
@@ -44,9 +55,9 @@ function RGBToHex(r, g, b) {
 function updatePreviewGrid() {
   const keyGrid = Array(rows).fill(0).map(() => Array(cols).fill(0));
   loadPixels();
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const index = (x + y * width) * 4;
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      const index = (x + y * cols) * 4;
       const [r, g, b] = pixels.slice(index, index + 3);
       previewGrid[y][x].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
       keyGrid[y][x] = RGBToHex(r, g, b);
